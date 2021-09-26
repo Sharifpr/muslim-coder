6844ce344c52d0965e43fb2a8b316f76b5e4dadc
Sharifpr <sharifpr532@gmail.com>
2021-09-26 20:12:42 +0600

    added

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -3,10 +3,11 @@ import './avatar.css';
 
 const Avatar = (props) => {
 
-    const { firstName, lastName } = props.user;
+    const { image, firstName, lastName } = props.user;
 
     return (
         <div className="avatar">
+            <img src={image} alt="" />
             <h5>{firstName} {lastName}</h5>
         </div>
     );
