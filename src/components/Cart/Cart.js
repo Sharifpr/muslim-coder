9654c9f5949a025839f63bc094aab91aa20a10d8
Sharifpr <sharifpr532@gmail.com>
2021-09-26 10:18:52 +0600

    added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import Avatar from '../Avatar/Avatar';
 import './cart.css';
 
@@ -8,7 +10,7 @@ const Cart = (props) => {
 
     return (
         <div className="cart">
-            <h2>Total Users added: {props.user.length}</h2>
+            <h2><FontAwesomeIcon className="icon" icon={faUser} /> Total Users added: {props.user.length}</h2>
             <h3>Total Salary: ${salary}</h3>
             <div className="avatar-container">
                 {
